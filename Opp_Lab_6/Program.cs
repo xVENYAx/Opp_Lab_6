f24bef58a46e56a0390764f6916b0ece034b5af5
Sasha <t3t3t3@ukr.net>
2021-05-24 20:00:16 +0300

    Добавив try, catch, finally.

--- Opp_Lab_6/Program.cs
+++ Opp_Lab_6/Program.cs
@@ -9,48 +9,110 @@ namespace Opp_Lab_6
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Ведіть 10 чисел, після кожного числа нажимайте enter:");
-            int k = Convert.ToInt32(Console.ReadLine());
-            int q = Convert.ToInt32(Console.ReadLine());
-            int w = Convert.ToInt32(Console.ReadLine());
-            int e = Convert.ToInt32(Console.ReadLine());
-            int r = Convert.ToInt32(Console.ReadLine());
-            int t = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
-            int u = Convert.ToInt32(Console.ReadLine());
-            int f = Convert.ToInt32(Console.ReadLine());
-            int o = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[10] { k, q, w, e, r, t, y, u, f, o};
-            int n = 10;
-            Console.WriteLine("Selection sort");
-            Console.Write("Initial array is: ");
-            for (int i = 0; i < n; i++)
+            try
             {
-                Console.Write(arr[i] + " ");
-            }
-            int temp, smallest;
-            for (int i = 0; i < n - 1; i++)
-            {
-                smallest = i;
-                for (int j = i + 1; j < n; j++)
+                Console.WriteLine("Ведіть 10 цілих чисел, після кожного числа нажимайте enter:");
+                int k = Convert.ToInt32(Console.ReadLine());
+                int q = Convert.ToInt32(Console.ReadLine());
+                int w = Convert.ToInt32(Console.ReadLine());
+                int e = Convert.ToInt32(Console.ReadLine());
+                int r = Convert.ToInt32(Console.ReadLine());
+                int t = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                int u = Convert.ToInt32(Console.ReadLine());
+                int f = Convert.ToInt32(Console.ReadLine());
+                int o = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[10] { k, q, w, e, r, t, y, u, f, o };
+                int n = 10;
+                Console.WriteLine();
+                Console.WriteLine("Selection sort");
+                Console.WriteLine();
+                Console.Write("Initial array is: ");
+                for (int i = 0; i < n; i++)
                 {
-                    if (arr[j] < arr[smallest])
-                    {
-                        smallest = j;
-                    }
+                    Console.Write(arr[i] + " ");
                 }
-                temp = arr[smallest];
-                arr[smallest] = arr[i];
-                arr[i] = temp;
+                int temp, smallest;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    smallest = i;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (arr[j] < arr[smallest])
+                        {
+                            smallest = j;
+                        }
+                    }
+                    temp = arr[smallest];
+                    arr[smallest] = arr[i];
+                    arr[i] = temp;
+                }
+                Console.WriteLine();
+                Console.Write("Sorted array is: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
-            Console.Write("Sorted array is: ");
-            for (int i = 0; i < n; i++)
+            
+            catch
             {
-                Console.Write(arr[i] + " ");
+                Console.WriteLine("Error! Ви не вели число, або не вели ціле число!");
+                Console.WriteLine("Ведіть знову число.");
+                Console.WriteLine();
+
+                Console.WriteLine("Ведіть 10 цілих чисел, після кожного числа нажимайте enter:");
+                int k = Convert.ToInt32(Console.ReadLine());
+                int q = Convert.ToInt32(Console.ReadLine());
+                int w = Convert.ToInt32(Console.ReadLine());
+                int e = Convert.ToInt32(Console.ReadLine());
+                int r = Convert.ToInt32(Console.ReadLine());
+                int t = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                int u = Convert.ToInt32(Console.ReadLine());
+                int f = Convert.ToInt32(Console.ReadLine());
+                int o = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[10] { k, q, w, e, r, t, y, u, f, o };
+                int n = 10;
+                Console.WriteLine();
+                Console.WriteLine("Selection sort");
+                Console.WriteLine();
+                Console.Write("Initial array is: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                int temp, smallest;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    smallest = i;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (arr[j] < arr[smallest])
+                        {
+                            smallest = j;
+                        }
+                    }
+                    temp = arr[smallest];
+                    arr[smallest] = arr[i];
+                    arr[i] = temp;
+                }
+                Console.WriteLine();
+                Console.Write("Sorted array is: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
-        }
+
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("End program.");
+            }
+            }
     }
 }
 
